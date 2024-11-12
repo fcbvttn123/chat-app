b01f91cd93ecee03942c7bf42da82a95953dd842
ngan <vutran@sheridancollege.ca>
2024-11-12 08:07:21 -0500

    React Router Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,19 @@
-function App() {}
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { ChatPage } from "./pages/ChatPage"
+import { RegisterAccount } from "./pages/RegisterAccount"
+import { Login } from "./pages/Login"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register-account" element={<RegisterAccount />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/chat-page" replace />} />
+        <Route path="/chat-page" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
---
+++ frontend/src/pages/RegisterAccount.jsx
@@ -0,0 +1,7 @@
+export function RegisterAccount() {
+  return (
+    <div>
+      <h1>Register Account</h1>
+    </div>
+  )
+}
