fda505cfa84fe211b1e2eca82549ca6f5c71d99e
ngan <vutran@sheridancollege.ca>
2024-11-15 11:06:10 -0500

    Login Function is finished

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,12 +5,15 @@ function createToken(_id) {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
 }
 
-function loginUser(req, res) {
-  res.json({
-    event: "Login User",
-    email: "abc@gmail.com",
-    password: "password",
-  })
+async function loginUser(req, res) {
+  const { username, password } = req.body
+  try {
+    const account = await AccountCollection.login(username, password)
+    const token = createToken(account._id)
+    res.status(200).json({ username, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 async function registerUser(req, res) {
