9228ae817f8b6eb570310df1b4150d9863b70281
ngan <vutran@sheridancollege.ca>
2024-11-15 11:02:08 -0500

    Account Model and Signup Logic is created

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,10 @@
+const jwt = require("jsonwebtoken")
+const AccountCollection = require("../models/user")
+
+function createToken(_id) {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
+}
+
 function loginUser(req, res) {
   res.json({
     event: "Login User",
@@ -6,12 +13,15 @@ function loginUser(req, res) {
   })
 }
 
-function registerUser(req, res) {
-  res.json({
-    event: "Register User",
-    email: "abc@gmail.com",
-    password: "password",
-  })
+async function registerUser(req, res) {
+  const { username, password } = req.body
+  try {
+    const account = await AccountCollection.signup(username, password)
+    const token = createToken(account._id)
+    res.status(200).json({ username, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 module.exports = {
