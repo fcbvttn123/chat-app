52d6e1842fa38b455799b991b6ada48912757d90
ngan <vutran@sheridancollege.ca>
2024-11-14 19:55:12 -0500

    Update flex box style for Separator component so that both lines take up the whole remaining space of flex box

--- frontend/src/components/Separator.jsx
+++ frontend/src/components/Separator.jsx
@@ -1,9 +1,9 @@
 export function Separator({ children }) {
   return (
     <div className="mt-[20px] mb-[14px] flex justify-center items-center gap-[5px] w-full">
-      <div className="h-[3px] w-full rounded-lg bg-[#666]"></div>
+      <div className="h-[3px] grow rounded-lg bg-[#666]"></div>
       <span className="text-xl text-[#555] font-extrabold">{children}</span>
-      <div className="h-[3px] w-full rounded-lg bg-[#666]"></div>
+      <div className="h-[3px] grow rounded-lg bg-[#666]"></div>
     </div>
   )
 }
