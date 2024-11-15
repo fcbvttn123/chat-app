8e60a5f46ca1cb358c1733e72f79eb65d681130b
ngan <vutran@sheridancollege.ca>
2024-11-14 20:03:20 -0500

    Add custom styles for Separator component

--- frontend/src/components/Separator.jsx
+++ frontend/src/components/Separator.jsx
@@ -1,6 +1,9 @@
-export function Separator({ children }) {
+export function Separator({ children, customStyles }) {
   return (
-    <div className="mt-[20px] mb-[14px] flex justify-center items-center gap-[5px] w-full">
+    <div
+      style={customStyles}
+      className="mt-[20px] mb-[14px] flex justify-center items-center gap-[5px] w-full"
+    >
       <div className="h-[3px] grow rounded-lg bg-[#666]"></div>
       <span className="text-xl text-[#555] font-extrabold">{children}</span>
       <div className="h-[3px] grow rounded-lg bg-[#666]"></div>
