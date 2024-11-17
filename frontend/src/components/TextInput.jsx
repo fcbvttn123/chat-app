b306ce856b5e13a9a629c15882da273a409cbdb0
ngan <vutran@sheridancollege.ca>
2024-11-16 21:26:37 -0500

    Add onChange and value property to TextInput component

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -1,10 +1,19 @@
-export function TextInput({ placeholder, name, type, customStyle }) {
+export function TextInput({
+  placeholder,
+  name,
+  type,
+  customStyle,
+  onChange,
+  value,
+}) {
   return (
     <input
       type={type}
       name={name}
       placeholder={placeholder}
       style={customStyle}
+      onChange={onChange}
+      value={value}
       className="bg-white w-full px-[15px] py-[6px] flex items-center justify-center gap-[5px] rounded-md border-2 border-solid border-[#323232] shadow-[4px_4px_#323232] overflow-hidden"
     />
   )
