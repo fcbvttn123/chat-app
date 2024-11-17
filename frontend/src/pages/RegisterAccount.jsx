53f1ef4694d4d31ab45dd7e1a0bac1682200c86d
ngan <vutran@sheridancollege.ca>
2024-11-16 22:15:18 -0500

    Add loading state variable for the register form

--- frontend/src/pages/RegisterAccount.jsx
+++ frontend/src/pages/RegisterAccount.jsx
@@ -5,17 +5,40 @@ import { TextInput } from "../components/TextInput"
 import { useState } from "react"
 
 export function RegisterAccount() {
+  // State Variables
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   })
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  // Functions
+  function formSubmitted(e) {
+    e.preventDefault()
+    setIsLoading(true)
+    console.log(formData)
+    setFormData({
+      username: "",
+      password: "",
+    })
+    setIsLoading(false)
+  }
+  function handleChange(e) {
+    const { name, value, type, checked } = event.target
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      }
+    })
+  }
   return (
     // This div element is the form container, mainly used to center the form
     // It has full width and height of the parent component (Layout)
     <div className="h-full w-full px-[50px] flex items-center justify-center">
       {/* Form Component */}
       <form
-        onSubmit={(e) => formSubmitted(e, formData, setFormData)}
+        onSubmit={formSubmitted}
         className="w-full max-w-[600px] p-5 bg-[#d3d3d3] flex flex-col items-start justify-center rounded-md border-2 border-solid border-[#323232] shadow-[4px_4px_#323232] font-mono"
       >
         <h1 className="text-2xl font-extrabold">Sign Up!</h1>
@@ -26,18 +49,20 @@ export function RegisterAccount() {
           name="username"
           type="text"
           placeholder="Username"
-          onChange={(e) => handleChange(e, setFormData)}
+          onChange={handleChange}
           value={formData.username}
         />
         <TextInput
           name="password"
           type="password"
           placeholder="Password"
-          onChange={(e) => handleChange(e, setFormData)}
+          onChange={handleChange}
           value={formData.password}
           customStyle={{ margin: "15px 0 15px 0" }}
         />
-        <Button type="submit">Create Account</Button>
+        <Button type="submit" disabled={isLoading}>
+          Create Account
+        </Button>
         <Separator customStyles={{ marginTop: "40px" }}>
           Already had an account?
         </Separator>
@@ -47,23 +72,4 @@ export function RegisterAccount() {
       </form>
     </div>
   )
-}
-
-function formSubmitted(e, formData, setFormData) {
-  e.preventDefault()
-  console.log(formData)
-  setFormData({
-    username: "",
-    password: "",
-  })
-}
-
-function handleChange(event, setFormData) {
-  const { name, value, type, checked } = event.target
-  setFormData((prevFormData) => {
-    return {
-      ...prevFormData,
-      [name]: type === "checkbox" ? checked : value,
-    }
-  })
 }
