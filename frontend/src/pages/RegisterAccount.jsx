e49f79ccc86576ca480d6e8afb1099f67166c0d0
ngan <vutran@sheridancollege.ca>
2024-11-18 19:25:37 -0500

    Code Refactor

--- frontend/src/pages/RegisterAccount.jsx
+++ frontend/src/pages/RegisterAccount.jsx
@@ -24,7 +24,7 @@ export function RegisterAccount() {
     setIsLoading(false)
   }
   function handleChange(e) {
-    const { name, value, type, checked } = event.target
+    const { name, value, type, checked } = e.target
     setFormData((prevFormData) => {
       return {
         ...prevFormData,
