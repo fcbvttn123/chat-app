95671241498ccc7064c6116a30f81a126e6844e9
ngan <vutran@sheridancollege.ca>
2024-11-23 22:11:39 -0500

    Working on the refresh bug

--- frontend/src/components/AuthRequired.jsx
+++ frontend/src/components/AuthRequired.jsx
@@ -1,12 +1,33 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Navigate, Outlet } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext"
 
 export function AuthRequired() {
   const { token } = useContext(AuthContext)
-  let authenticated = token
-  if (!authenticated) {
-    return <Navigate to="/login" />
+  const [loading, setLoading] = useState(true)
+  const [authenticated, setAuthenticated] = useState(false)
+  fetch(`${import.meta.env.VITE__BACKEND_URL}/api/auth/verifyToken`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      if (data.status === "Valid Token") {
+        setAuthenticated(true)
+      }
+      setLoading(false)
+    })
+  if (loading) {
+    console.log("loading")
+    return <div>Loading...</div>
+  } else {
+    if (!authenticated) {
+      return <Navigate to="/login" />
+    }
+    return <Outlet />
   }
-  return <Outlet />
 }
