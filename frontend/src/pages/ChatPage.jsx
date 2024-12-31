b0815a2e9bd9c090b0fac2724fd74f89e43ad7f8
ngan <vutran@sheridancollege.ca>
2024-12-30 21:43:03 -0500

    Cleaning code for frontend

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,12 +1,71 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../context/AuthContext"
+import {
+  useCreateChatClient,
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+  ChannelList,
+} from "stream-chat-react"
+import "stream-chat-react/dist/css/v2/index.css"
+import { callPostAPI } from "../utils/functions"
 
 export function ChatPage() {
   const { username } = useContext(AuthContext)
-  console.log(username)
+  const [streamChatToken, setStreamChatToken] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const client = useCreateChatClient({
+    apiKey: import.meta.env.VITE__STREAM_CHAT_API_KEY,
+    tokenOrProvider: streamChatToken,
+    userData: { id: username },
+  })
+
+  useEffect(() => {
+    async function getStreamChatToken(username) {
+      if (username) {
+        const response = await callPostAPI("/api/auth/createStreamChatToken", {
+          username,
+        })
+        if (response?.error) {
+          setError(response?.error)
+        } else {
+          setStreamChatToken(response?.streamChatToken)
+        }
+      }
+      setLoading(false)
+    }
+    getStreamChatToken(username)
+  }, [username])
+
+  if (loading || !client) return <div>Setting up client & connection...</div>
+  if (error) return <div>{error}</div>
   return (
-    <div>
-      <h1>Chat Page</h1>
+    <div className="flex h-full">
+      <Chat client={client}>
+        <ChannelList
+          filters={{
+            type: "messaging",
+            members: { $in: [username] },
+          }}
+          sort={{ last_message_at: -1 }}
+          options={{
+            limit: 10,
+          }}
+        />
+        <Channel>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   )
 }
