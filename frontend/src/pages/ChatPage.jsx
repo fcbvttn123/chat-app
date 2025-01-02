05cc62d203a99ed2190b08f44e9a879e1e712753
ngan <vutran@sheridancollege.ca>
2025-01-01 22:00:50 -0500

    Installing clsx from npm and add a custom component for channel list item

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,5 +1,8 @@
-import { useContext, useEffect, useState } from "react"
+// Imports for React
+import { useContext, useEffect, useRef, useState } from "react"
 import { AuthContext } from "../context/AuthContext"
+import { callPostAPI } from "../utils/functions"
+// Imports for StreamChat
 import {
   useCreateChatClient,
   Chat,
@@ -10,9 +13,97 @@ import {
   Thread,
   Window,
   ChannelList,
+  useComponentContext,
 } from "stream-chat-react"
 import "stream-chat-react/dist/css/v2/index.css"
-import { callPostAPI } from "../utils/functions"
+import { ChannelPreviewActionButtons as DefaultChannelPreviewActionButtons } from "stream-chat-react"
+import clsx from "clsx"
+
+function CustomListItem(props) {
+  const {
+    active,
+    Avatar = DefaultAvatar,
+    channel,
+    className: customClassName = "",
+    displayImage,
+    displayTitle,
+    groupChannelDisplayInfo,
+    latestMessagePreview,
+    onSelect: customOnSelectChannel,
+    setActiveChannel,
+    unread,
+    watchers,
+  } = props
+
+  const { ChannelPreviewActionButtons = DefaultChannelPreviewActionButtons } =
+    useComponentContext()
+
+  const channelPreviewButton = useRef(null)
+
+  const avatarName =
+    displayTitle ||
+    channel.state.messages[channel.state.messages.length - 1]?.user?.id
+
+  function onSelectChannel(e) {
+    if (customOnSelectChannel) {
+      customOnSelectChannel(e)
+    } else if (setActiveChannel) {
+      setActiveChannel(channel, watchers)
+    }
+    if (channelPreviewButton?.current) {
+      channelPreviewButton.current.blur()
+    }
+  }
+
+  return (
+    <div className="str-chat__channel-preview-container">
+      <ChannelPreviewActionButtons channel={channel} />
+      <button
+        aria-label={`Select Channel: ${displayTitle || ""}`}
+        aria-selected={active}
+        className={clsx(
+          `str-chat__channel-preview-messenger str-chat__channel-preview`,
+          active && "str-chat__channel-preview-messenger--active",
+          unread &&
+            unread >= 1 &&
+            "str-chat__channel-preview-messenger--unread",
+          customClassName
+        )}
+        data-testid="channel-preview-button"
+        onClick={onSelectChannel}
+        ref={channelPreviewButton}
+        role="option"
+      >
+        <div className="str-chat__channel-preview-messenger--left">
+          <Avatar
+            className="str-chat__avatar--channel-preview"
+            groupChannelDisplayInfo={groupChannelDisplayInfo}
+            image={displayImage}
+            name={avatarName}
+          />
+        </div>
+        <div className="str-chat__channel-preview-end">
+          <div className="str-chat__channel-preview-end-first-row">
+            <div className="str-chat__channel-preview-messenger--name">
+              <span>{displayTitle}</span>
+            </div>
+            {!!unread && (
+              <div
+                className="str-chat__channel-preview-unread-badge"
+                data-testid="unread-badge"
+              >
+                {unread}
+              </div>
+            )}
+          </div>
+          <div className="str-chat__channel-preview-messenger--last-message">
+            {latestMessagePreview}
+          </div>
+        </div>
+      </button>
+    </div>
+  )
+}
 
 export function ChatPage() {
   const { username } = useContext(AuthContext)
@@ -48,6 +139,7 @@ export function ChatPage() {
     <div className="flex h-full">
       <Chat client={client}>
         <ChannelList
+          Preview={CustomListItem}
           filters={{
             type: "messaging",
             members: { $in: [username] },
